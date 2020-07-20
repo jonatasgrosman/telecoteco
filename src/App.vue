09c053e06b5c07a405baa9230583f7ee30a74926
Jonatas Grosman <grosman.jonatas@gmail.com>
2020-07-20 01:19:03 -0300

    app refactoring + deploy config

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,72 @@
 <template>
   <v-app>
+    <v-navigation-drawer v-model="drawer" app overflow>
+      <Menu />
+    </v-navigation-drawer>
 
-    <v-app-bar app color="primary" dark>
-      <v-toolbar-title>Ta-Tu-Ti-Tu-TÁ</v-toolbar-title>
+    <v-app-bar app>
+      <v-row>
+        <v-col md="auto">
+          <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        </v-col>
+        <v-col align="right">
+          <inline-svg
+            style="margin-left:10px; cursor:pointer;"
+            width="100"
+            height="50"
+            :src="require(`../public/assets/icons/logo_big8.svg`)"
+            @click="goToHello()"
+          />
+        </v-col>
+      </v-row>
+      
     </v-app-bar>
 
     <v-main>
       <v-container class="fill-height" fluid>
-
         <v-row align="center">
           <v-col>
-            <Player/>
+            <Hello v-if="pageType == 'HELLO'"/>
+            <Exercise v-if="pageType == 'EXERCISE'"/>
+            <References v-if="pageType == 'REFERENCES'"/>
           </v-col>
         </v-row>
-
-        <v-row align="center">
-          <v-col>
-            <Panels/>
-          </v-col>
-        </v-row>
-
       </v-container>
     </v-main>
+
+    <v-footer inset app>
+      <Player />
+    </v-footer>
   </v-app>
 </template>
 
 <script>
+import { mapMutations, mapGetters } from "vuex";
+import Hello from "./components/Hello.vue";
+import Player from "./components/Player.vue";
+import Menu from "./components/Menu.vue";
+import Exercise from "./components/Exercise.vue";
+import References from "./components/References.vue";
 
-  import Panels from './components/Panels.vue';
-  import Player from './components/Player.vue';
-
-  export default {
-    components: {
-      Panels,
-      Player
-    },
-    data: () => ({}),
-  }
+export default {
+  methods: {
+    ...mapMutations(['goToHello'])
+  },
+  computed: {
+    ...mapGetters(["pageType"]),
+  },
+  components: {
+    Hello,
+    Exercise,
+    Player,
+    Menu,
+    References
+  },
+  data: () => ({
+    drawer: null
+  })
+};
 </script> 
 
 <style scoped>
-
 </style>
