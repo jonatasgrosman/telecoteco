7233074bba912606a03b8e7916bf9efeae717640
Georges Spyrides <georgesmss@gmail.com>
2020-07-20 13:18:11 -0300

    about page prototype

---
+++ src/components/About.vue
@@ -0,0 +1,44 @@
+<template>
+
+  <div>
+
+    <v-layout
+      text-xs-center
+      wrap
+    >
+      <v-flex xs12>
+        <v-img
+          :src="require('../../public/assets/icons/logo_big8.svg')"
+          class="my-3"
+          contain
+          height="200"
+        ></v-img>
+      </v-flex>
+
+      <v-flex mb-4>
+        <h1 class="display-2 font-weight-bold mb-3">
+          <!--<a href="https://grosmanjs.github.io/telecoteco/" target="_blank">telecote.co</a>-->
+          telecote.co
+        </h1>
+        <p class="heading font-weight-regular">
+          Sobre o projeto
+        </p>
+        <p>
+          A ideia do telecote.co é ser uma brincadeira, uma porta de entrada para leitura de partituras e uma referência básica de gêneros e ritmos brasileiros.
+        </p>
+        <p>
+          To-Do
+        </p>
+      </v-flex>
+
+      
+    </v-layout>
+  </div>
+  
+</template>
+
+<script>
+</script> 
+
+<style scoped>
+</style>
